8c2f5bf654a91d94a4d452f859dfff5f6dcfca69
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-03-22 10:42:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, str) {
+    return drivers.filter(driver => driver.toLowerCase() === str.toLowerCase());
+  }
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(function(driver) {
+      return driver.startsWith(query);
+    });
+  }
+  
+function matchName(drivers, query) {
+    return drivers.filter(function(driver) {
+      return driver.name === query;
+    });
+  }
+  
